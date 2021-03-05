66e2d2fc8d039a42d84b40d8deff898263d46d0e
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-05 17:55:51 +0200

    SRS with styled components

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,7 +1,36 @@
 import React from "react";
 import Document, { Html, Head, NextScript, Main } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  //SERVER SIDE STYLED COMPONENT https://styled-components.com/docs/advanced#server-side-rendering
+  // with NEXT.js
+  //https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
   render() {
     return (
       <Html lang="en-en">
