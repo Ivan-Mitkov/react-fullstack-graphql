bb9a70bccff3457b0eff9f0523133eb8b5e5cc29
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-05 17:25:21 +0200

    add nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import PropTypes from 'prop-types';
-import Page from '../components/Page';
+import PropTypes from "prop-types";
+import Page from "../components/Page";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "../components/styles/nprogress.css";
+
+//https://ricostacruz.com/nprogress/
+//https://nextjs.org/docs/api-reference/next/router#routerevents
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Document, { Html, Head, NextScript, Main } from 'next/document';
+import React from "react";
+import Document, { Html, Head, NextScript, Main } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-en">
-        {/* <Head /> */}
+        <Head />
         <body>
           <Main />
           <NextScript />
