a20ef549b2c90520fda9e2390374032eece89530
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-05 15:00:17 +0200

    add nav component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import Link from "next/link";
+import Nav from "./Nav";
 const Header = () => (
   <header>
-    <div className="bar"></div>
+    <div className="bar">
+      <Link href="/">Sick Fit</Link>
+    </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
+    <Nav />
   </header>
 );
 
