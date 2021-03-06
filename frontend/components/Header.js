74b5a9831768bd67e46c1473f0d3e16f2949a419
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-06 18:08:45 +0200

    fix nav

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -25,7 +25,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -40,11 +40,11 @@ const Header = () => (
       <Logo>
         <Link href="/">Sick Fit </Link>
       </Logo>
+      <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <Nav />
   </HeaderStyles>
 );
 
