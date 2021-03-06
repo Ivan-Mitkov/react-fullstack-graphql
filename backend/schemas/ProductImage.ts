26d0d6784a73c61e023b97898068e26fd4f1acc1
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-06 13:24:33 +0200

    data relationship between product and productimage

---
+++ backend/schemas/ProductImage.ts
@@ -0,0 +1,26 @@
+import { list } from "@keystone-next/keystone/schema";
+import { cloudinaryImage } from "@keystone-next/cloudinary";
+import { text, relationship } from "@keystone-next/fields";
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: "sicfits",
+};
+//https://next.keystonejs.com/apis/schema
+export const ProductImage = list({
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: "Source",
+    }),
+    altText: text(),
+    product: relationship({ ref: `Product.photo` }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ["image", "altText", "product"],
+    },
+  },
+});
