fc459a7cf59bf28737f628d4981821acefd6e500
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-06 10:23:16 +0200

    setup mongodb atlas and keystone.js

---
+++ backend/keystone.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits";
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30, //how long should stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL,
+    //TODO: add data seeding
+  },
+  lists: createSchema({
+    //schema items go here
+  }),
+  ui: {
+    //TODO:change this for roles
+    isAccessAllowed: () => true,
+  },
+  //TODO: add session values here
+});
