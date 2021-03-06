8c626c07d155834e1086869f25d0325a68223729
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-06 15:56:09 +0200

    Apollo setup

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,13 +1,14 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
-import { onError } from '@apollo/link-error';
-import { getDataFromTree } from '@apollo/client/react/ssr';
-import { createUploadLink } from 'apollo-upload-client';
-import withApollo from 'next-with-apollo';
-import { endpoint, prodEndpoint } from '../config';
+import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/link-error";
+import { getDataFromTree } from "@apollo/client/react/ssr";
+import { createUploadLink } from "apollo-upload-client";
+import withApollo from "next-with-apollo";
+import { endpoint, prodEndpoint } from "../config";
 
 function createClient({ headers, initialState }) {
   return new ApolloClient({
     link: ApolloLink.from([
+      //https://www.apollographql.com/docs/react/api/link/apollo-link-error/
       onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
           graphQLErrors.forEach(({ message, locations, path }) =>
@@ -20,11 +21,12 @@ function createClient({ headers, initialState }) {
             `[Network error]: ${networkError}. Backend is unreachable. Is it running?`
           );
       }),
-      // this uses apollo-link-http under the hood, so all the options here come from that package
+      // this uses apollo-link-http under the hood, so all the options here come from that package https://github.com/jaydenseric/apollo-upload-client#function-formdataappendfile this package allow FILE UOLOAD
+
       createUploadLink({
-        uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
         fetchOptions: {
-          credentials: 'include',
+          credentials: "include",
         },
         // pass the headers along from this request. This enables SSR with logged in state
         headers,
@@ -43,4 +45,5 @@ function createClient({ headers, initialState }) {
   });
 }
 
+//https://www.npmjs.com/package/next-with-apollo withApollo receives 2 parameters, the first one is a function that returns the Apollo Client The getDataFromTree function takes your React tree, determines which queries are needed to render them, and then fetches them all.
 export default withApollo(createClient, { getDataFromTree });
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,8 @@ import Page from "../components/Page";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "../components/styles/nprogress.css";
+import { ApolloProvider } from "@apollo/client";
+import withData from "../lib/withData";
 
 //https://ricostacruz.com/nprogress/
 //https://nextjs.org/docs/api-reference/next/router#routerevents
@@ -11,11 +13,26 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+//if any page has getInitialProps fetch it It's better to add Apollo in every page instead if you have pages that don't need Apollo. https://www.npmjs.com/package/next-with-apollo
+// To access Apollo Client in each page's getInitialProps, add getInitialProps to App like so:
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return pageProps;
+};
+//https://www.apollographql.com/docs/react/api/react/hooks/#the-apolloprovider-component
+//apollo prop comes from withData function wich wrap the app
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
 
@@ -23,3 +40,4 @@ MyApp.propTypes = {
   Component: PropTypes.any,
   pageProps: PropTypes.object,
 };
+export default withData(MyApp);
