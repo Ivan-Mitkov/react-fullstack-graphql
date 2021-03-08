30252729f07776e6a75014bb6c3e0559cf052ba0
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-08 15:15:15 +0200

    pagination - create component get number of products and pages

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,43 @@
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import Head from "next/head";
+import Link from "next/link";
+import PaginationStyles from "./styles/PaginationStyles";
+import ErrorMessage from "./ErrorMessage";
+import { perPage } from "../config";
+
+const ALL_PRODUCTS_COUNT = gql`
+  query ALL_PRODUCTS_COUNT {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+const Pagination = ({ page }) => {
+  const { loading, error, data } = useQuery(ALL_PRODUCTS_COUNT);
+  // console.log(data);
+  if (loading) return "";
+  if (error) return <ErrorMessage error={error}></ErrorMessage>;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick - Fits | Page - {page}</title>
+      </Head>
+      <Link href={`/page/${page - 1}`}>
+        <a aria-disabled={page <= 1}>Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <Link href={`/page/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next</a>
+      </Link>
+      <p>{count} Items Total</p>
+    </PaginationStyles>
+  );
+};
+
+export default Pagination;
--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Products from "../components/Products";
+import Pagination from "../components/Pagination";
 const ProductsPage = () => {
   return (
     <div>
+      <Pagination page={7}></Pagination>
       <Products />
+      <Pagination page={1}></Pagination>
     </div>
   );
 };
