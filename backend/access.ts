22923395a0a6e287aacae3bef680532a5c2b4693
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-16 16:22:48 +0200

    permissions - checkboxes

--- backend/access.ts
+++ backend/access.ts
@@ -2,8 +2,26 @@
 //they have acces to the context and inside context we have acces to the user session
 //they must return true or false
 
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
-export function isSignedIn({ session }:ListAccessArgs) {
+export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((p) => [
+    p,
+    ({ session }: ListAccessArgs) => !!session?.data.role?.[p],
+  ])
+);
+//Permission check if somebody meets a criteria yes/no
+export const permissions = {
+  ...generatedPermissions,
+  // canManageProducts({ session }) {
+  //   return session?.data.role?.canManageProducts;
+  // }, and ... all others
+};
+
+//Rule based functions - used for lists access 
+
